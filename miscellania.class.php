e3d15d1288c63df257637efc5727c98fcfc4e112
CancerDaPolimig <gustavocastrol@hotmail.com>
2015-07-12 23:18:46 -0300

    Gabriel Oliveira e Giaccomo
    Pares

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,5 +1,16 @@
 <?php
 class Miscellania {
 
+public $n;
+
+ public function Par($n){
+  if($n % 2 == 0) {
+   echo "O numero eh par";
+  }
+  else {
+   echo "O numero não eh par";
+  }
+ }
+
 }
 ?>
